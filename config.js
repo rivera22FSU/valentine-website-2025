40efe9077d06d21fe701d41df6a4efa977e37d03
rivera22FSU <rivera22@ferris.edu>
2026-02-12 23:45:17 -0500

    Update config.js

--- config.js
+++ config.js
@@ -49,8 +49,8 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "YES! I LOVE YOU SO MUCH SILLY BUTT!!! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "I hope you enjoy your gift :) !",
+        title: "YES! I LOVE YOU SO MUCH SILLY BUTT!!! YOU MAKE ME SO HAPPY! 🎉💝💖💝💓",
+        message: "I hope you enjoy your gift!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "C:\Users\jrwar\Downloads\Olivia Dean - I've Seen It (Lyrics).mp3", // Music streaming URL
+        musicUrl: "https://soundcloud.com/oliviaadean/ive-seen-it", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
